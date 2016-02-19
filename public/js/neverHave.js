e9df4550c74438713347a5e43092f8a238657fea
Dilraj Singh <dilrajsingh.7@gmail.com>
2016-02-18 20:18:39 -0800

    work on Never Have I ever

--- public/js/neverHave.js
+++ public/js/neverHave.js
@@ -10,4 +10,27 @@ $(document).ready(function() {
  */
 function initializePage() {
 	// add any functionality and listeners you want here
+
+	$('#post1btn').click(function(){
+
+		var input = $('#post1input').val();
+		
+		console.log(input);
+		
+		$('#post1').hide();
+		
+		/*var $template = $('#neverHave-questionCard').html();
+		var template = Handlebars.compile($template);
+		var data = {};
+		data['test'] = input;
+		var output = template(data);*/
+
+		$('.page-content').append(input);
+
+		playAI();
+	});
+}
+
+function playAI() {
+	console.log('AI');
 }
